60edb7e1a1e3e83b7aeca0190928766bc9b582b1
Niko Fink <nfink95@gmail.com>
2020-05-17 17:01:23 +0200

    new component meta system

--- src/ics/converter/component.py
+++ src/ics/converter/component.py
@@ -7,90 +7,15 @@ from attr import Attribute
 from ics.converter.base import AttributeConverter, GenericConverter
 from ics.grammar import Container
 from ics.types import ContainerItem, ContextDict
+from ics.utils import check_is_instance
 
 if TYPE_CHECKING:
     from ics.component import Component
 
 
 @attr.s(frozen=True)
-class ComponentMeta(object):
-    container_name: str = attr.ib()
-    converter_class: Type["ComponentConverter"] = attr.ib(default=None)
-
-    def inflate(self, component_type: Type["Component"]):
-        if component_type.Meta is not self:
-            raise ValueError("can't inflate %s for %s, it's meta is %s" % (self, component_type, component_type.Meta))
-        converters = cast(Iterable["AttributeConverter"], filter(bool, (
-            AttributeConverter.get_converter_for(a)
-            for a in attr.fields(component_type)
-        )))
-        component_type.Meta = InflatedComponentMeta(
-            component_type=component_type,
-            converters=tuple(sorted(converters, key=lambda c: c.priority)),
-            container_name=self.container_name,
-            converter_class=self.converter_class or ComponentConverter)
-
-    def load_instance(self, container: Container, context: Optional[ContextDict] = None):
-        raise NotImplementedError("this is only available for InflatedComponentMeta, was Component.__init_subclass__ called?")
-
-    def populate_instance(self, instance: "Component", container: Container, context: Optional[ContextDict] = None):
-        raise NotImplementedError("this is only available for InflatedComponentMeta, was Component.__init_subclass__ called?")
-
-    def serialize_toplevel(self, component: "Component", context: Optional[ContextDict] = None):
-        raise NotImplementedError("this is only available for InflatedComponentMeta, was Component.__init_subclass__ called?")
-
-
-@attr.s(frozen=True)
-class InflatedComponentMeta(ComponentMeta):
-    converters: Tuple[GenericConverter, ...] = attr.ib(default=None)
-    component_type: Type["Component"] = attr.ib(default=None)
-
-    converter_lookup: Dict[str, List[GenericConverter]]
-
-    def __attrs_post_init__(self):
-        object.__setattr__(self, "converter_lookup", defaultdict(list))
-        for converter in self.converters:
-            for name in converter.filter_ics_names:
-                self.converter_lookup[name].append(converter)
-
-    def __call__(self, attribute: Attribute):
-        return self.converter_class(attribute, self)
-
-    def load_instance(self, container: Container, context: Optional[ContextDict] = None):
-        instance = self.component_type()
-        self.populate_instance(instance, container, context)
-        return instance
-
-    def populate_instance(self, instance: "Component", container: Container, context: Optional[ContextDict] = None):
-        if container.name != self.container_name:
-            raise ValueError("container isn't an {}".format(self.container_name))
-        if not context:
-            context = ContextDict(defaultdict(lambda: None))
-
-        for line in container:
-            consumed = False
-            for conv in self.converter_lookup[line.name]:
-                if conv.populate(instance, line, context):
-                    consumed = True
-            if not consumed:
-                instance.extra.append(line)
-
-        for conv in self.converters:
-            conv.finalize(instance, context)
-
-    def serialize_toplevel(self, component: "Component", context: Optional[ContextDict] = None):
-        if not context:
-            context = ContextDict(defaultdict(lambda: None))
-        container = Container(self.container_name)
-        for conv in self.converters:
-            conv.serialize(component, container, context)
-        container.extend(component.extra)
-        return container
-
-
-@attr.s(frozen=True)
-class ComponentConverter(AttributeConverter):
-    meta: InflatedComponentMeta = attr.ib()
+class MemberComponentConverter(AttributeConverter):
+    meta: "ComponentMeta" = attr.ib()
 
     @property
     def filter_ics_names(self) -> List[str]:
@@ -109,3 +34,76 @@ class ComponentConverter(AttributeConverter):
             raise ValueError("ComponentConverter %s can't serialize extra params %s", (self, extras))
         for value in self.get_value_list(parent):
             output.append(self.meta.serialize_toplevel(value, context))
+
+
+EMPTY_CONVERTERS = tuple()
+
+
+@attr.s(frozen=True)
+class ComponentMeta(object):
+    container_name: str = attr.ib()
+    component_type: Type["Component"] = attr.ib()
+
+    converter_class: Type[MemberComponentConverter] = attr.ib(default=MemberComponentConverter)
+    converters: Tuple[GenericConverter, ...] = attr.ib(default=EMPTY_CONVERTERS)
+
+    converter_lookup: Dict[str, List[GenericConverter]]
+
+    def __attrs_post_init__(self):
+        if self.converters is EMPTY_CONVERTERS:
+            converters = cast(Iterable["AttributeConverter"], filter(bool, (
+                AttributeConverter.get_converter_for(a) for a in attr.fields(self.component_type))))
+        else:
+            converters = self.converters
+        object.__setattr__(self, "converters", tuple(sorted(converters, key=lambda c: c.priority)))
+
+        object.__setattr__(self, "converter_lookup", defaultdict(list))
+        for converter in self.converters:
+            for name in converter.filter_ics_names:
+                self.converter_lookup[name].append(converter)
+
+        self.component_type.Meta = self
+        AttributeConverter.BY_TYPE[self.component_type] = self
+
+    def __call__(self, attribute: Attribute):
+        converter_class = self.converter_class or MemberComponentConverter
+        return converter_class(attribute, self)
+
+    def load_instance(self, container: Container, context: Optional[ContextDict] = None):
+        instance = self.component_type()
+        self.populate_instance(instance, container, context)
+        return instance
+
+    def populate_instance(self, instance: "Component", container: Container, context: Optional[ContextDict] = None):
+        if container.name != self.container_name:
+            raise ValueError("container isn't an {}".format(self.container_name))
+        check_is_instance("instance", instance, self.component_type)
+        if not context:
+            context = ContextDict(defaultdict(lambda: None))
+
+        self._populate_attrs(instance, container, context)
+
+    def _populate_attrs(self, instance: "Component", container: Container, context: ContextDict):
+        for line in container:
+            consumed = False
+            for conv in self.converter_lookup[line.name]:
+                if conv.populate(instance, line, context):
+                    consumed = True
+            if not consumed:
+                instance.extra.append(line)
+
+        for conv in self.converters:
+            conv.finalize(instance, context)
+
+    def serialize_toplevel(self, component: "Component", context: Optional[ContextDict] = None):
+        check_is_instance("instance", component, self.component_type)
+        if not context:
+            context = ContextDict(defaultdict(lambda: None))
+        container = Container(self.container_name)
+        self._serialize_attrs(component, context, container)
+        return container
+
+    def _serialize_attrs(self, component: "Component", context: ContextDict, container: Container):
+        for conv in self.converters:
+            conv.serialize(component, container, context)
+        container.extend(component.extra)
